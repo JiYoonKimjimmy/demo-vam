2940202fd4192e2c391ed91b6aa925d1a73d9dea
jimmyberg <jimmyberg.kim@gmail.com>
2024-06-07 17:30:32 +0900

    feat: custom column converter 적용

--- module-core/src/main/kotlin/com/konai/vam/core/virtualaccount/repository/entity/VirtualAccountEntity.kt
+++ module-core/src/main/kotlin/com/konai/vam/core/virtualaccount/repository/entity/VirtualAccountEntity.kt
@@ -1,6 +1,8 @@
 package com.konai.vam.core.virtualaccount.repository.entity
 
+import com.konai.vam.core.common.converter.DatabaseCustomerInfoColumnConverter
 import com.konai.vam.core.common.entity.BaseEntity
+import jakarta.persistence.Convert
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
@@ -11,6 +13,7 @@ class VirtualAccountEntity(
     @Id
     @GeneratedValue
     val id: Long? = null,
+    @Convert(converter = DatabaseCustomerInfoColumnConverter::class)
     val accountNumber: String,
     val bankCode: String,
     val bankName: String,
