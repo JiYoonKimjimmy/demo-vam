0447d2fda73ac7bfc14fbec31bb9ca5dc640a582
jimmyberg <jimmyberg.kim@gmail.com>
2024-06-11 17:19:58 +0900

    feat: module-batch properties 설정 변경

---
+++ module-batch/src/main/kotlin/com/konai/vam/batch/config/VamBatchApplicationConfig.kt
@@ -0,0 +1,10 @@
+package com.konai.vam.batch.config
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@EntityScan(basePackages = ["com.konai.vam.core"])
+@ComponentScan(basePackages = ["com.konai.vam.core"])
+@Configuration
+class VamBatchApplicationConfig
