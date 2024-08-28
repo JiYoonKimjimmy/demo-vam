cf9fbf2db9373afa8e4caad29c3cf82c5c56cd81
jimmyberg <jimmyberg.kim@gmail.com>
2024-08-28 16:57:12 +0900

    chore: 코드 정리

--- module-core/src/main/kotlin/com/konai/vam/core/config/VamCoreApplicationConfig.kt
+++ module-core/src/main/kotlin/com/konai/vam/core/config/VamCoreApplicationConfig.kt
@@ -3,13 +3,9 @@ package com.konai.vam.core.config
 import com.konasl.commonlib.logging.filter.EnableLoggingFilter
 import com.konasl.commonlib.logging.helper.masking.MaskingProperties
 import com.konasl.commonlib.springweb.correlation.EnableCorrelationFilter
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
 import org.springframework.transaction.annotation.EnableTransactionManagement
-import java.util.*
 
 @EnableLoggingFilter
 @EnableCorrelationFilter
