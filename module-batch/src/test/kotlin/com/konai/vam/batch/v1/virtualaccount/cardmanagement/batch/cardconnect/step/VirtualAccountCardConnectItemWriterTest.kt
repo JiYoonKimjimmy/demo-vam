448c4f54d13f3942c4b6b769bffa81156b84f81a
jimmyberg <jimmyberg.kim@gmail.com>
2024-09-06 17:29:35 +0900

    feat: sync project

--- module-batch/src/test/kotlin/com/konai/vam/batch/v1/virtualaccount/cardmanagement/batch/cardconnect/step/VirtualAccountCardConnectItemWriterTest.kt
+++ module-batch/src/test/kotlin/com/konai/vam/batch/v1/virtualaccount/cardmanagement/batch/cardconnect/step/VirtualAccountCardConnectItemWriterTest.kt
@@ -1,35 +1,42 @@
 package com.konai.vam.batch.v1.virtualaccount.cardmanagement.batch.cardconnect.step
 
 import VirtualAccountCardConnectItemFixture
+import com.konai.vam.batch.v1.virtualaccount.cardmanagement.batch.cardconnect.step.item.VirtualAccountCardConnectItem
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.batch.item.Chunk
 import org.springframework.batch.item.ExecutionContext
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 class VirtualAccountCardConnectItemWriterTest : BehaviorSpec({
 
     val virtualAccountCardConnectItemFixture = VirtualAccountCardConnectItemFixture()
 
-    xgiven("VirtualAccountCardConnectItemWriter") {
-        val batchId = "00000800029500017071809205815I00001"
+    given("가상 계좌 카드 연결 전문 파일 생성 요청하여") {
+        val tempDir = Files.createTempDirectory("vam-test")
+        val filePath = tempDir.resolve("test_output.txt").toString()
+        val batchId = "TEST001"
         val quantity = 1
+        val writer = VirtualAccountCardConnectItemWriter(filePath, batchId, quantity)
 
-        val tempDir = Paths.get(System.getProperty("user.dir"))
-        val tempFile = tempDir.resolve("raw_data_additional_$batchId.SAM")
-        val writer = VirtualAccountCardConnectItemWriter(tempDir.toString(), batchId, quantity)
+        val items = Chunk(listOf(virtualAccountCardConnectItemFixture.make()))
 
-        `when`("writing data to a file") {
+        `when`("'Temp' 디렉토리 정상 파일 생성된 경우") {
             writer.open(ExecutionContext())
-            writer.write(Chunk(listOf(virtualAccountCardConnectItemFixture.make())))
+            writer.write(items)
             writer.close()
 
-            then("부가전문에 대한 데이터를 포멧에 맞춰 파일이 갖고 있다.") {
-                val lines = Files.readAllLines(tempFile)
+            then("파일 내용 정상 확인한다") {
+                val lines = Files.readAllLines(Path.of(filePath))
+                lines.size shouldBe 3
                 lines[0] shouldBe "BatchId=$batchId"
                 lines[1] shouldBe "Quantity=00000$quantity"
-                lines[2] shouldBe "000001SVCID=123456789012345;par=123456789012345678901234567;VIRTUALACC=1234567890123456;"
+                lines[2] shouldBe "000001SVCID=123456789012345;PAR=123456789012345678901234567;VIRTUALACC=1234567890123456    ;"
+
+                // Clean up temporary directory
+                tempDir.toFile().deleteRecursively()
             }
         }
     }
