0819bd960aff86dd55ef4b2c2b1ea6bc5af0bbbd
jimmyberg <jimmyberg.kim@gmail.com>
2024-09-18 22:49:31 +0900

    feat: sync project

--- module-batch/src/main/kotlin/com/konai/vam/batch/v1/virtualaccount/cardmanagement/batch/cardconnect/step/VirtualAccountCardConnectItemReader.kt
+++ module-batch/src/main/kotlin/com/konai/vam/batch/v1/virtualaccount/cardmanagement/batch/cardconnect/step/VirtualAccountCardConnectItemReader.kt
@@ -2,11 +2,10 @@ package com.konai.vam.batch.v1.virtualaccount.cardmanagement.batch.cardconnect.s
 
 import com.konai.vam.core.common.EMPTY
 import com.konai.vam.core.repository.virtualaccount.entity.VirtualAccountEntity
-import jakarta.persistence.EntityManager
 import jakarta.persistence.EntityManagerFactory
 import jakarta.persistence.Query
 import org.springframework.batch.item.database.JpaPagingItemReader
-import org.springframework.batch.item.database.orm.JpaQueryProvider
+import org.springframework.batch.item.database.orm.AbstractJpaQueryProvider
 
 class VirtualAccountCardConnectItemReader(
     entityManagerFactory: EntityManagerFactory,
@@ -18,19 +17,15 @@ class VirtualAccountCardConnectItemReader(
         this.name = this::class.java.simpleName
         this.pageSize = chunkSize
         this.setEntityManagerFactory(entityManagerFactory)
-        setQueryProvider(VirtualAccountQueryProvider(entityManagerFactory, batchId))
+        this.setQueryProvider(VirtualAccountQueryProvider(batchId))
     }
 
 }
 
-class VirtualAccountQueryProvider(
-    private val entityManagerFactory: EntityManagerFactory,
-    private val batchId: String
-) : JpaQueryProvider {
+class VirtualAccountQueryProvider(private val batchId: String) : AbstractJpaQueryProvider() {
 
     override fun createQuery(): Query {
-        val em = entityManagerFactory.createEntityManager()
-        val query = em.createQuery(buildQuery(), VirtualAccountEntity::class.java)
+        val query = entityManager.createQuery(buildQuery(), VirtualAccountEntity::class.java)
         if (batchId.isNotBlank()) {
             query.setParameter("batchId", batchId)
         }
@@ -46,6 +41,6 @@ class VirtualAccountQueryProvider(
         """.trimIndent()
     }
 
-    override fun setEntityManager(entityManager: EntityManager) {}
+    override fun afterPropertiesSet() {}
 
 }
